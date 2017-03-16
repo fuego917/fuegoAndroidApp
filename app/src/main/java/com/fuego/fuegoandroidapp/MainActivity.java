766843d2fb0f15461feadb81557e2b75782f4eee
indetail-akada-ttm <akada-ttm@indetail.co.jp>
2017-03-16 15:24:15 +0900

    listview added

--- app/src/main/java/com/fuego/fuegoandroidapp/MainActivity.java
+++ app/src/main/java/com/fuego/fuegoandroidapp/MainActivity.java
@@ -3,11 +3,23 @@ package com.fuego.fuegoandroidapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+//final ArrayList<String> data = new ArrayList<>();
+//data.add("札幌"); data.add("釧路");
+
+//ArrayAdapter<String> adapter = new ArrayAdapter<> (this, android.R.layout.simple_list_item_1, data);
+//        ListView list = (ListView) findViewById(R.id.list);
+//        list.setAdapter(adapter);
+
 }
+}
+
+
