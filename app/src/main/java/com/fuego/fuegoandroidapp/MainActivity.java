94d21fa2f14cddd391552524367b5ddeb9c7bc6f
indetail-akada-ttm <akada-ttm@indetail.co.jp>
2017-03-30 21:12:56 +0900

    city list added

--- app/src/main/java/com/fuego/fuegoandroidapp/MainActivity.java
+++ app/src/main/java/com/fuego/fuegoandroidapp/MainActivity.java
@@ -12,14 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//final ArrayList<String> data = new ArrayList<>();
-//data.add("札幌"); data.add("釧路");
-
-//ArrayAdapter<String> adapter = new ArrayAdapter<> (this, android.R.layout.simple_list_item_1, data);
-//        ListView list = (ListView) findViewById(R.id.list);
-//        list.setAdapter(adapter);
-
-}
+    }
 }
 
 
